283baa64f19fcf5b0f83cc82127b82021477a5fc
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-08-20 11:21:12 +0530

    app navbar setup

--- apps/conduit/src/app/app.module.ts
+++ apps/conduit/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
---
+++ libs/ngrx-forms/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: '../../jest.preset.js',
+  coverageDirectory: '../../coverage/libs/ngrx-forms',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  displayName: 'ngrx-forms',
+};
